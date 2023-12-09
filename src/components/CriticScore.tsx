3d1260b1617138e9dfeffaabe4cfe33f17a88566
Ali kashani <alikashani092@gmail.com>
2023-12-09 04:49:42 +0330

    Displaying critic score

---
+++ src/components/CriticScore.tsx
@@ -0,0 +1,20 @@
+import { Badge } from "@chakra-ui/react";
+
+interface Props {
+    score: number;
+}
+const CriticScore = ({ score }: Props) => {
+    const color = score > 70 ? "green" : score > 60 ? "yellow" : "red";
+    return (
+        <Badge
+            fontSize="14px"
+            paddingX={2}
+            borderRadius="6px"
+            colorScheme={color}
+        >
+            {score}
+        </Badge>
+    );
+};
+
+export default CriticScore;
